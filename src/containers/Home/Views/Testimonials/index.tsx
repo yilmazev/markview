09d8dd3a4bec90fdd0ee38af91c71751d3691479
Yılmaz Ev <yilmaz.ev@outlook.com>
2024-02-16 15:09:54 +0300

    testimonials h2 text updated

--- src/containers/Home/Views/Testimonials/index.tsx
+++ src/containers/Home/Views/Testimonials/index.tsx
@@ -7,7 +7,7 @@ const Testimonials: React.FC = () => {
     return (
         <div className="mb-12 mt-6 flex flex-col items-center justify-center lg:mb-24 lg:mt-12">
             <div className="flex flex-col items-center">
-                <h2 className="mb-2 text-center text-lg font-semibold md:text-3xl lg:mb-4 lg:text-4xl">
+                <h2 className="mb-2 text-center text-xl font-semibold md:text-3xl lg:mb-4 lg:text-4xl">
                     An Adored Tool by Over <span className="!bg-clip-text [-webkit-text-fill-color:#0000] [background:linear-gradient(0deg,#b1a5eb,#a594fd_90%)] [text-fill-color:#0000]">100</span> Users</h2>
                 <p className="max-w-lg text-center text-sm font-light text-white/75 lg:text-base">
                     Dive into the experiences of individuals who have given Markview a try. Discover the stories that showcase the meaningful impact this tool has made in the lives of our users!
