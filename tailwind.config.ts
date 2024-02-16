eb21ea9eaee9b492e1e5c98be2d2dec9c0a84f94
Yılmaz Ev <yilmaz.ev@outlook.com>
2024-02-16 12:49:35 +0300

    tailwind bug fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss"
 
 const config: Config = {
     content: [
+        "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
